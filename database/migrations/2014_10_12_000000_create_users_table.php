eba3dd588fef73846a937198382f7f8802fb8b82
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:38:30 -0300

    Add User base table structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,23 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
+    public function up(){
+        Schema::create('users', function (Blueprint $table){
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('profile_id')->unsigned();
+            $table->bigInteger('default_role_id')->unsigned()->nullable()->default(null); //Default role id, can be overwritten when assigned to a project
+            $table->boolean('active')->default(true);
             $table->string('password');
+            $table->string('avatar')->unique()->nullable(); //null = Use default
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); //created_at and updated_at
+            $table->softDeletes(); //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -29,8 +34,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('users');
     }
 }
