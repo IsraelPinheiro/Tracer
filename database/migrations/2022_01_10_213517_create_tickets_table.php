c53b1e501e9761f623e241a77d5e52f0f1459061
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:45:52 -0300

    Add Ticket base table structure

--- database/migrations/2022_01_10_213517_create_tickets_table.php
+++ database/migrations/2022_01_10_213517_create_tickets_table.php
@@ -4,18 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketsTable extends Migration
-{
+class CreateTicketsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('tickets', function (Blueprint $table) {
+    public function up(){
+        Schema::create('tickets', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+			$table->string('title');
+			$table->text('description');
+			$table->text('solution')->nullable()->default(null);
+			$table->bigInteger('project_id')->unsigned();
+			$table->bigInteger('author_id')->unsigned();
+			//Priority Matrix : Priority = Likelihood X Severity = Very Low, Low, Moderate, High, Critical
+			$table->smallInteger('likelihood');	//Very Low:1, Low:2,  Medium:3, High:4, Very High:5
+			$table->smallInteger('severity');	//Very Low:1, Low:2,  Medium:3, High:4, Critical:5
+			$table->smallInteger('status');		//New:1, Assigned:2, In Progress:3, Pending:4, On-hold:5, Solved:6, Closed:7
+			$table->smallInteger('type_id');
+			$table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+			$table->bigInteger('created_by')->unsigned();
+			$table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +35,7 @@ class CreateTicketsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('tickets');
     }
 }
