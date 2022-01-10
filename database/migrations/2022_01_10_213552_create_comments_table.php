ffe2d47c4bc3e428400903533317b46606ae3fb6
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:50:59 -0300

    Add Comments table base structure

--- database/migrations/2022_01_10_213552_create_comments_table.php
+++ database/migrations/2022_01_10_213552_create_comments_table.php
@@ -4,18 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
-{
+class CreateCommentsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('comments', function (Blueprint $table) {
+    public function up(){
+        Schema::create('comments', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('commentable_type');
+            $table->bigInteger('commentable_id')->unsigned();
+            $table->text('comment')->nullable()->default(null);
+            $table->boolean('private')->default(true);
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +29,7 @@ class CreateCommentsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('comments');
     }
 }
