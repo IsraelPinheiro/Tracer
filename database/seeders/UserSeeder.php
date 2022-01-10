f4dbcbfebcd6d57fd48adb1394393a7fb32d199c
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:54:21 -0300

    Add Users seeding data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,16 +3,34 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
-class UserSeeder extends Seeder
-{
+class UserSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        DB::table('users')->delete();
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@tracer.com.br',
+            'password' => Hash::make('admin'),
+            'profile_id' => 1,
+            'created_by' => 1
+        ]);
+        $this->command->info('User Administrador created');
+
+        User::create([
+            'name' => 'Usuário',
+            'email' => 'user@tracer.com.br',
+            'password' => Hash::make('user'),
+            'profile_id' => 2,
+            'created_by' => 1
+        ]);
+        $this->command->info('User Usuário created');
     }
 }
