0a0b8a39b4564db0b41b5dc836e9161a48961d9a
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:47:31 -0300

    Add Tasks table base structure

--- database/migrations/2022_01_10_213532_create_tasks_table.php
+++ database/migrations/2022_01_10_213532_create_tasks_table.php
@@ -4,18 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
-{
+class CreateTasksTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('tasks', function (Blueprint $table) {
+    public function up(){
+        Schema::create('tasks', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+			$table->string('title');
+			$table->text('description');
+            $table->bigInteger('project_id')->unsigned();
+            $table->dateTime('deadline')->nullable()->default(null);
+			$table->smallInteger('priority');   // Very Low:1, Low:2, Moderate:3, High:4, Critical:5
+			$table->smallInteger('status');     //New:1, Assigned:2, In Progress:3, On-hold:4, Canceled:5, Closed:6, Declined:7
+
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+			$table->bigInteger('created_by')->unsigned();
+			$table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +32,7 @@ class CreateTasksTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('tasks');
     }
 }
