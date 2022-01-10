895a24384f6990d8752a11894118e75652c68da0
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:43:05 -0300

    Add Change Log base table structure

--- database/migrations/2022_01_10_213507_create_change_logs_table.php
+++ database/migrations/2022_01_10_213507_create_change_logs_table.php
@@ -4,18 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChangeLogsTable extends Migration
-{
+class CreateChangeLogsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('change_logs', function (Blueprint $table) {
+    public function up(){
+        Schema::create('change_logs', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned();
+            //Polymorphic relation
+            $table->string('loggable_type');
+            $table->bigInteger('loggable_id')->unsigned();
+            $table->enum('action', ['create', 'read', 'update', 'delete']);
+            $table->json('old_data')->nullable()->default(null);
+            $table->timestamp('created_at');
         });
     }
 
@@ -24,8 +28,7 @@ class CreateChangeLogsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('change_logs');
     }
 }
