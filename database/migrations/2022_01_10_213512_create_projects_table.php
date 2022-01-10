781ffd7dff65a33ae9f155943674014b2663d0be
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:45:42 -0300

    Add Project base table structure

--- database/migrations/2022_01_10_213512_create_projects_table.php
+++ database/migrations/2022_01_10_213512_create_projects_table.php
@@ -4,18 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsTable extends Migration
-{
+class CreateProjectsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('projects', function (Blueprint $table) {
+    public function up(){
+        Schema::create('projects', function (Blueprint $table){
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable()->default(null);
+            $table->bigInteger('manager_id')->unsigned();
+            $table->bigInteger('owner_id')->unsigned()->nullable()->default(null);
+            $table->string('project_repository')->nullable()->default(null);
+            $table->string('project_page')->nullable()->default(null);
             $table->timestamps();
+            $table->softDeletes(); //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +31,7 @@ class CreateProjectsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('projects');
     }
 }
