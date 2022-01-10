6b8d629ff3bfd2cb4564483b2234ec9ed7131945
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:46:03 -0300

    Add Ticket Template base table structure

--- database/migrations/2022_01_10_213522_create_ticket_templates_table.php
+++ database/migrations/2022_01_10_213522_create_ticket_templates_table.php
@@ -4,18 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketTemplatesTable extends Migration
-{
+class CreateTicketTemplatesTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('ticket_templates', function (Blueprint $table) {
+    public function up(){
+        Schema::create('ticket_templates', function (Blueprint $table){
             $table->id();
+            $table->string('name')->unique();
+            //Template Data
+            $table->string('template_title')->nullable()->default(null);
+            $table->text('template_description')->nullable()->default(null);
+			//Priority Matrix : Priority = Likelihood X Severity = Very Low, Low, Moderate, High, Critical
+			$table->smallInteger('template_likelihood');	//Very Low:1, Low:2,  Medium:3, High:4, Very High:5
+			$table->smallInteger('template_severity');		//Very Low:1, Low:2,  Medium:3, High:4, Critical:5
+			$table->smallInteger('template_type');
+
             $table->timestamps();
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +34,7 @@ class CreateTicketTemplatesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('ticket_templates');
     }
 }
