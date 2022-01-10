2391d0b1f08315394ba71118b13ff7b666fe36f4
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:47:44 -0300

    Add Ticket Types table base structure

--- database/migrations/2022_01_10_213527_create_ticket_types_table.php
+++ database/migrations/2022_01_10_213527_create_ticket_types_table.php
@@ -4,18 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketTypesTable extends Migration
-{
+class CreateTicketTypesTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('ticket_types', function (Blueprint $table) {
+    public function up(){
+        Schema::create('ticket_types', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->text('description')->nullable()->default(null);
+            $table->bigInteger('icon_id')->unsigned()->nullable()->default(null); //Null = Default
+            $table->string('color',6)->nullable()->default(null);
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+			$table->bigInteger('created_by')->unsigned();
+			$table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +29,7 @@ class CreateTicketTypesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('ticket_types');
     }
 }
