b81ac25e3b8f9539c952ab37c1d63648b2a77486
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:50:24 -0300

    Add Notifications table base structure

--- database/migrations/2022_01_10_213556_create_notifications_table.php
+++ database/migrations/2022_01_10_213556_create_notifications_table.php
@@ -4,17 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotificationsTable extends Migration
-{
+class CreateNotificationsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('notifications', function (Blueprint $table) {
+    public function up(){
+        Schema::create('notifications', function (Blueprint $table){
             $table->id();
+            $table->bigInteger('to_id')->unsigned();
+            $table->bigInteger('from_id')->unsigned()->nullable()->default(null); //null = Notification from the system
+            $table->text('text')->nullable()->default(null);
+            $table->enum('type', ['primary', 'alert', 'success', 'danger', 'warning']);
+            $table->boolean('read')->default(false);
             $table->timestamps();
         });
     }
@@ -24,8 +27,7 @@ class CreateNotificationsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('notifications');
     }
 }
