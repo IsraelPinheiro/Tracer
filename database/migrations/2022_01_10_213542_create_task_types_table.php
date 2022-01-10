517d20f6badd016a7a00142a6d727fd78626a8dc
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:50:42 -0300

    Add Task Types table base structure

--- database/migrations/2022_01_10_213542_create_task_types_table.php
+++ database/migrations/2022_01_10_213542_create_task_types_table.php
@@ -4,18 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTaskTypesTable extends Migration
-{
+class CreateTaskTypesTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('task_types', function (Blueprint $table) {
+    public function up(){
+        Schema::create('task_types', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->text('description')->nullable()->default(null);
+            $table->bigInteger('icon_id')->unsigned()->nullable()->default(null); //Null = Default
+            $table->string('color',6)->nullable()->default(null);
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+			$table->bigInteger('created_by')->unsigned();
+			$table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +29,7 @@ class CreateTaskTypesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('task_types');
     }
 }
