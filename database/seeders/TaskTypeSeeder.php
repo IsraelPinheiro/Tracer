87d04f5de1bc0a2812ce5b5e7cd08ca0dc41ca6d
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 20:54:43 -0300

    Add Task Types seeding data

--- database/seeders/TaskTypeSeeder.php
+++ database/seeders/TaskTypeSeeder.php
@@ -2,17 +2,37 @@
 
 namespace Database\Seeders;
 
+use App\Models\TaskType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class TaskTypeSeeder extends Seeder
-{
+class TaskTypeSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        DB::table('task_types')->truncate();
+        TaskType::create([
+            'name' => 'Bug',
+			'description' => 'Tarefa relacionada a correção de bugs',
+            'color' => 'FF99FF',
+            'created_by' => 1
+        ]);
+
+        TaskType::create([
+            'name' => 'Recurso',
+			'description' => 'Tarefa relacionada a criação de novos recursos',
+            'color' => 'FF99FF',
+            'created_by' => 1
+        ]);
+
+        TaskType::create([
+            'name' => 'Conteúdo',
+			'description' => 'Tarefa relacionada a criação de novo conteúdo',
+            'color' => 'FF99FF',
+            'created_by' => 1
+        ]);
     }
 }
