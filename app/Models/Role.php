65f769e13b4f096eec358035f4d2d4458a2e7e9b
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 21:04:33 -0300

    Add relation to users with this role as it's default role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
-{
+class Role extends Model{
     use HasFactory;
+
+    //Relationships
+    public function default_users(){
+        return $this->hasMany(User::class, 'default_role_id');
+    }
 }
