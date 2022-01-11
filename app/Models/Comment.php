5fb08f0de7d129db84dbb55eeffb5e9a8b904ac2
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 21:52:20 -0300

    Add commentable polymorphic relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
+class Comment extends Model{
     use HasFactory;
+
+    //Relationships
+    //A Comment can belong to a Ticket or Task
+    public function commentable(){
+        return $this->morphTo();
+    }
 }
