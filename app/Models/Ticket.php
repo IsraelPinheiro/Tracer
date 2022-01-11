c21d9b1f85f0d97ac64d2edf2105baad584e6fb3
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 22:01:04 -0300

    Add Ticket/TicketType relation

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ticket extends Model
-{
+class Ticket extends Model{
     use HasFactory;
+
+    //Relationships
+    //Ticket's Type
+    public function type(){
+        return $this->belongsTo(TicketType::class);
+    }
 }
--- app/Models/TicketType.php
+++ app/Models/TicketType.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TicketType extends Model
-{
+class TicketType extends Model{
     use HasFactory;
+
+    //Relationships
+    //Type's Tyckets
+    public function tickets(){
+        return $this->hasMany(Ticket::class);
+    }
 }
