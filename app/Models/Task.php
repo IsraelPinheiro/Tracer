7cfcb9418954c64a449444c2d4ec7337ad3eadbc
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 21:59:40 -0300

    Add Task/TaskType Relation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
-{
+class Task extends Model{
     use HasFactory;
+
+    //Relationships
+    //Task's Type
+    public function type(){
+        return $this->belongsTo(TaskType::class);
+    }
 }
--- app/Models/TaskType.php
+++ app/Models/TaskType.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TaskType extends Model
-{
+class TaskType extends Model{
     use HasFactory;
+
+    //Relationships
+    //Type's Tasks
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
