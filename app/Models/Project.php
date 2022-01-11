a01b068fcebb65bae994de0ed190f75bd9a7c411
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 21:56:56 -0300

    Add relation to project's owner and manager

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
-{
+class Project extends Model{
     use HasFactory;
+
+    //Relationships
+    //Project's Comments
+    public function comments(){
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+    //Project's Attachments
+    public function attachments(){
+        return $this->morphMany(Attachment::class, 'attachable');
+    }
+    //Project's Owner
+    public function owner(){
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+    //Projects Manager
+    public function manager(){
+        return $this->belongsTo(User::class, 'manager_id');
+    }
 }
