7c8607e6143de38a339d577270cc55bf8946e06a
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 21:05:21 -0300

    Add logic for timestamps management

--- app/Models/AccessLog.php
+++ app/Models/AccessLog.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AccessLog extends Model
-{
+class AccessLog extends Model{
     use HasFactory;
+
+    const CREATED_AT = 'accessed_at';
+    public $timestamps = false;
+    public function setCreatedAtAttribute($value){ 
+        $this->attributes['created_at'] = \Carbon\Carbon::now(); 
+    }
+    protected $casts = [
+        'accessed_at' => 'datetime',
+    ];
 }
