a63bb16e7dcaeabe1592e8acb2b601b311f17c4e
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 21:01:00 -0300

    Add users() relationship

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
-{
+class Profile extends Model{
     use HasFactory;
+
+    //Relationships
+    //Profile's Users
+    public function users(){
+        return $this->hasMany(User::class);
+    }
 }
