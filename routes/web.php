1c1cbaa3bed3d8e449c098b179f215fd7a6ef9dd
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-10 22:38:22 -0300

    Set root as home and protect route

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
