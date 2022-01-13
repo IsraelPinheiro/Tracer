3dd58c783b617de37e0e3a525b093c8d4d270826
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-13 20:07:23 -0300

    Add new plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,64 @@
-require('./bootstrap');
+//Base and initialization
+require('./bootstrap')
+//Dashmix
 require('./dashmix')
-
 //Datatables + Plugins
-require('./datatables');
-//Fullcalendar
+require('./datatables')
+//Datatables Initialization
+require('./datatables.init')
+//Fullcalendar - https://fullcalendar.io
 require('./fullcalendar')
+//CKEditor - https://ckeditor.com
+require('@ckeditor/ckeditor5-build-classic')
+require('@ckeditor/ckeditor5-build-inline')
+//Bootstrap Colorpicker - https://itsjavi.com/bootstrap-colorpicker/
+require('bootstrap-colorpicker')
+//Bootstrap Datepicker - https://github.com/uxsolutions/bootstrap-datepicker
+require('bootstrap-datepicker')
+//Bootstrap MaxLength - https://github.com/mimo84/bootstrap-maxlength
+require('bootstrap-maxlength')
+//Bootstrap Notify - https://github.com/mouse0270/bootstrap-notify
+require('bootstrap-notify')
+//Chart.js - https://www.chartjs.org
+require('chart.js')
+//Cropper.js - https://fengyuanchen.github.io/cropperjs/
+require('cropperjs')
+//Dropzone - https://www.dropzone.dev/js/
+require('dropzone')
+//Easy pie chart - http://rendro.github.io/easy-pie-chart/
+require('easy-pie-chart')
+//Flatpickr - https://flatpickr.js.org
+require('flatpickr')
+//highlight.js - https://highlightjs.org
+require('highlightjs')
+//Ion.RangeSlider - http://ionden.com/a/plugins/ion.rangeSlider/
+require('ion-rangeslider')
+//jQuery appear - https://github.com/morr/jquery.appear
+require('jquery.appear')
+//jquery Masked Input - https://github.com/excellalabs/jquery.maskedinput#readme
+require('jquery.maskedinput')
+////jQuery Countdown - https://github.com/kemar/jquery.countdown
+require('jquery-countdown')
+//jQuery Sparkline - https://omnipotent.net/jquery.sparkline/#s-about
+require('jquery-sparkline')
+//jQuery Validation - https://jqueryvalidation.org
+require('jquery-validation')
+//jVectorMap - https://jvectormap.com
+require('jvectormap')
+//Magnific Popup - https://dimsemenov.com/plugins/magnific-popup/
+require('magnific-popup')
+//Nestable2 - https://github.com/RamonSmit/Nestable2
+require('nestable2')
+//Raty - https://github.com/wbotelhos/raty
+require('raty-js')
+//Select2 - https://select2.org
+require('select2')
+//SimpleMDE Markdown Editor - https://simplemde.com
+require('simplemde')
+//Slick Carousel - https://kenwheeler.github.io/slick/
+require('slick-carousel')
+//SweetAlert 2 - https://sweetalert2.github.io
+require('sweetalert2')
+//Vide - http://vodkabears.github.io/vide/
+require('vide')
+//require('pwstrength-bootstrap/dist/pwstrength-bootstrap')
