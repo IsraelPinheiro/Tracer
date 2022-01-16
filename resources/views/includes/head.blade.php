3eb0cd7cce171d5f5df0b50f56ec743e07340c5f
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-15 22:19:55 -0300

    Set title complement

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,7 +3,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	{{-- CSRF Token --}}
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Laravel') }} - @yield('title-complement')</title>
 	<meta name="description" content="A small, but reliable, bugs and tasks tracking system, with some project management features">
 	<meta name="author" content="Israel R Pinheiro">
 	<meta name="robots" content="noindex, nofollow">
